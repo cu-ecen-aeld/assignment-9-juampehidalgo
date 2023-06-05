a2a841b3b3530366d936c5b2377eb2e686a9203e
juampehidalgo <juanpedro.hidalgo@hotmail.com>
2023-06-05 11:24:47 +0300

    Initial commit to add package ldd and make sure that branches are correct

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,29 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+LDD_VERSION = '#COMMIT VERSION NUMBER'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = '#GITHUB REPOSITORY LINK'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+define LDD_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
