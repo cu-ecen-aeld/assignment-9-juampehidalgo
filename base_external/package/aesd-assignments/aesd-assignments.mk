216d8f6c911f99a88f8396d25d2c257381b20c5c
juampehidalgo <juanpedro.hidalgo@hotmail.com>
2023-06-25 09:51:50 +0300

    Updated hashes to driver and server implementations

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 2faa1c943ed85af9ac3698bb93514e386002fe7d
+AESD_ASSIGNMENTS_VERSION = 1821ebab67bda8d6aeb8dcd0c3dad23080097b96
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
